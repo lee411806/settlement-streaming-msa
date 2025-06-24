e16ce8b7eb47d9d6a7bc2d714c8822a00a61dbbd
JDragon <lee411806@naver.com>
2025-06-24 13:18:35 +0900

    init commit

---
+++ src/main/java/com/jaeyong/streamingmsa/StreamingMsaApplication.java
@@ -0,0 +1,13 @@
+package com.jaeyong.streamingmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaeyong/streamingmsa/StreamingMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeyong.streamingmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
