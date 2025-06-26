88a25dcd1a6ab41cb74c7b3938f9191894c7cc8c
JDragon <lee411806@naver.com>
2025-06-26 15:11:01 +0900

    feat: Config Server 및 Eureka 연동 설정 추가

--- src/main/java/com/jaeyong/streamingmsa/StreamingMsaApplication.java
+++ src/main/java/com/jaeyong/streamingmsa/StreamingMsaApplication.java
@@ -2,8 +2,10 @@ package com.jaeyong.streamingmsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StreamingMsaApplication {
 
     public static void main(String[] args) {
